1747df5646e2c9e7667bbe6945a1cc8135c3bf1e
bajtyibity <angelofgodcode@gmail.com>
2022-12-20 09:49:05 +0100

    first commit

---
+++ Controllers/LAB5/SomeController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication3.Services.LAB5;
+
+namespace WebApplication3.Controllers.LAB5
+{
+
+    [ApiController]
+    [Route("[controller]/[action]")]
+    public class SomeController : ControllerBase
+    {
+        [HttpGet]
+        public string CallMethod()
+        {
+            var someInstance = new SomeClass();
+            return someInstance.Print(null);
+        }
+    }
+
+}
---
+++ Services/LAB5/SomeClass.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication3.Services.LAB5
+{
+    public class SomeClass
+    {
+           public string Print(string text)
+           {
+                try
+                {
+                    return text.Length.ToString();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    throw new InvalidDepartmentException("CustomException");
+                }
+           
+           }
+    }
+}
